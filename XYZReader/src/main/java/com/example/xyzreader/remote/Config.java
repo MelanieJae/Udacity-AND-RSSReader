0fdea1a62b2d58a5dfbbe6ba5dfb2d59ac3686b5
Melanie Hand <mjhanddev@gmail.com>
2017-03-17 14:48:29 -0700

    changes: add layouts for 800w portrait and landscape, edit layout for main screen list cards; add getLayoutManager to list activity
    resolves issues: all changes resolve layout issues to accommodate various screen sizes

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -0,0 +1,22 @@
+package com.example.xyzreader.remote;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import timber.log.Timber;
+
+public class Config {
+    public static final URL BASE_URL;
+
+    static {
+        URL url = null;
+        try {
+            url = new URL("https://raw.githubusercontent.com/Protino/dump/master/Lego/data.json");
+//            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
+        } catch (MalformedURLException ignored) {
+            Timber.e("Error: Malformed URL");
+        }
+
+        BASE_URL = url;
+    }
+}
